131a81bd82df9d1b4e5e9406ed7866eb42a95180
Physix <lolathecat16@outlook.com>
2024-07-26 23:16:30 -0600

    updated mw3, black ops, mw2, world at war, and cod 4 pages

--- Call of Duty HQ/Views/CoD4MWPage.xaml.cs
+++ Call of Duty HQ/Views/CoD4MWPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Call_of_Duty_HQ.ViewModels;
+﻿using System.Diagnostics;
+using Call_of_Duty_HQ.ViewModels;
 
 using Microsoft.UI.Xaml.Controls;
 
@@ -15,5 +16,10 @@ public sealed partial class CoD4MWPage : Page
     {
         ViewModel = App.GetService<CoD4MWViewModel>();
         InitializeComponent();
+    }
+
+    private void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        Process.Start("C:\\Program Files (x86)\\Steam\\steam.exe", "steam://rungameid/1938090");
     }
 }
