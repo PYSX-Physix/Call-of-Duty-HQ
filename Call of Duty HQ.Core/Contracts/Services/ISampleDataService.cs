a62b72f02bacdac9bd33540cb83476b2d1e7f3a7
Physix <108170666+PYSX-Physix@users.noreply.github.com>
2024-09-19 21:17:53 -0600

    Delete Call of Duty HQ.Core directory

--- Call of Duty HQ.Core/Contracts/Services/ISampleDataService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Call_of_Duty_HQ.Core.Models;
-
-namespace Call_of_Duty_HQ.Core.Contracts.Services;
-
-// Remove this class once your pages/features are using your data.
-public interface ISampleDataService
-{
-    Task<IEnumerable<SampleOrder>> GetListDetailsDataAsync();
-}
