623709c9e5ca1b5e54ffb0cc2287f5e42a72b1c5
PYSX-Phsyix <lolathecat16@outlook.com>
2024-09-23 20:00:44 -0600

    Initial Commit

---
+++ Call of Duty HQ/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Call_of_Duty_HQ.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ Call of Duty HQ/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Call_of_Duty_HQ.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
