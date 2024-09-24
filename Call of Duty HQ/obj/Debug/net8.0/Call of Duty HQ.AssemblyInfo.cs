e61ec43944ed0ab378ae70dfd463d0a8a54f943e
PYSX-Phsyix <lolathecat16@outlook.com>
2024-09-23 20:13:11 -0600

    Building new Launcher

--- Call of Duty HQ/obj/Debug/net8.0/Call of Duty HQ.AssemblyInfo.cs
+++ Call of Duty HQ/obj/Debug/net8.0/Call of Duty HQ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Call of Duty HQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+623709c9e5ca1b5e54ffb0cc2287f5e42a72b1c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f6021d006746435ad507d3fd8fd74d0555ec25b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Call of Duty HQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Call of Duty HQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
